87a4a4297314c5efdf4da739906e07fb04471d65
Hossam Hasan <codingworld5000@gmail.com>
2025-11-01 14:32:48 +0200

    start working on the load function

--- script.js
+++ script.js
@@ -1,38 +1,60 @@
 // vars & cons
-const storedData = JSON.parse(localStorage.getItem("data")) || [];
-saveBtn = document.querySelector(".btn-save");
-ingredName = document.querySelector(".ingred-name");
-ingredValue = document.querySelector(".ingred-value");
+const saveBtn = document.querySelector(".btn-save");
+const ingredName = document.querySelector(".ingred-name");
+const ingredValue = document.querySelector(".ingred-value");
+let dataBase = [];
 
 // functions
 function save() {
+    // vars & cons
     const name = ingredName.value.trim();
     const value = ingredValue.value.trim();
-    const ingred = {
-        "ingred": name,
+    const ingredObj = {
+        "name": name,
         "value": value,
-    };
-
-    if (name && value) {
-        // add new ingreds
-        storedData.push(ingred);
-        localStorage.setItem("data", JSON.stringify(storedData));
-
-        // clear inputs
-        ingredName.value = "";
-        ingredValue.value = "";
     }
-    else {
-        console.log("please enter any value")
+
+    // ensure that data is filled
+    if (!name) {
+        alert("name field is required");
+        return;
     }
+
+    if (!value.length || isNaN(value)) {
+        alert("Please, enter a valid value!");
+        return;
+    }
+
+    // load dataBase
+    dataBase = JSON.parse(localStorage.getItem("data")) || [];
+
+    // push the savedObj to the dataBase 
+    dataBase.push(ingredObj);
+
+    // save to the dataBase
+    localStorage.setItem("data", JSON.stringify(dataBase));
+
+    // clear input fields
+    ingredName.value = "";
+    ingredValue.value = "";
+
+    // test
+    console.log(value.length)
 }
 
+function load() {
+    // vars & cons
+    dataBase = JSON.parse(localStorage.getItem("data")) || [];
 
+    // test the logic
+    dataBase.forEach(ele => {
+        const storedName = ele.name;
+        const storedValue = ele.value;
+
+        console.log(storedName, storedValue);
+    });
+}
 // events
 saveBtn.addEventListener("click", save);
-window.addEventListener("load", () => {
 
-});
 
-// test
-console.log(storedData)
