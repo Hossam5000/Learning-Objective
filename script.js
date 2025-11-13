131d751496ce3d268bc3b4079232afb40727992f
Hossam <codingWorld5000@gmail.com>
2025-11-13 05:32:42 +0200

    starting a new mini skills repo

---
+++ script.js
@@ -0,0 +1,10 @@
+// vars & cons
+const Lis = document.querySelectorAll("ul li");
+const selected = document.querySelector(".selected");
+
+// logic
+selected.style.backgroundColor = "green";
+selected.style.color = "white";
+
+
+console.log(selected.getElementByName);
